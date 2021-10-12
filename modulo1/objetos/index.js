459a6c958321dd1b3d8c5e438a5f2beddf167812
George Carneiro <george@gmail.com>
2021-10-11 23:35:48 -0300

    Exercício objetos, faltou desenvolver a 2b e 3

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,81 @@
+// --------------------------- Objetos ----------------------------------
+// Exercícios de interpretação de código
+
+// 1.
+// a)  Matheus Nachtergaele, 21h
+//     Virginia Cavendish
+//     Canal: Globo 
+//     horario: 14h
+
+// 2.
+// a) nome: Juca
+//    idade: 3
+//    raça: "SRD"
+//
+//    nome: Juba
+//    idade: 3 
+//    raça: "SRD"
+//   
+//    nome: Jubo
+//    idade: 3
+//    raça: "SRD"
+//
+// b) Copia o conteúdo da função anterior
+//
+// 3.
+// a) false
+//    underfined
+//
+// b) Acessou o objeto PESSOA e acessou o valor da propriedade BACKENDER
+//    que é falsa, logo depois tentou acessar o valor da propriedade
+//    ALTURA, porem ela não foi definida(undefined).
+
+// -------------- Exercícios de escrita de código -----------------------
+
+// 1.
+//a)
+
+const pessoa = {
+  nome: "Amanda",
+  apelidos: ["Amandinha", "Mandinha", "Mandi"] 
+  
+}
+  console.log(`Eu sou Amanda, mas pode me chamar de: ${pessoa.apelidos}`)
+
+// b)
+
+  
+const pessoa2 = {...pessoa, novosApelidos: ["Dev", "RockGirl", "Programadora"]
+
+}
+console.log(pessoa2)
+
+
+
+//2.
+//a)
+const formulario = {
+  nome: "Walt Whitman",
+  idade: 50,
+  profissao: "Escritor"
+
+}
+const formulario2 = {
+  nome: "Charles Bukowski",
+  idade: 40,
+  profissao: "Poeta"
+}
+
+// b)
+function receberObjeto (formulario) {
+  
+
+  return 
+  
+}
+
+//
+//
+//
+
+
